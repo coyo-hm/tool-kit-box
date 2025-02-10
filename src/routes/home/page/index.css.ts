a78479dfb35164d66cca13aac1b532c9d255d1e9
COYO <dayeon.yoon@gmail.com>
2025-02-11 02:25:05 +0900

    feat: main page

---
+++ src/routes/home/page/index.css.ts
@@ -0,0 +1,10 @@
+import { style } from "@vanilla-extract/css";
+
+export const linkContainerStyle = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "stretch",
+  justifyContent: "center",
+  gap: "1rem",
+  padding: "2rem",
+});
--- src/routes/home/page/index.tsx
+++ src/routes/home/page/index.tsx
@@ -1,41 +1,16 @@
 import { Link as ReactLink, LinkProps } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { linkContainerStyle } from "./index.css.ts";
+import { buttonStyle } from "@components/Button/button.css.ts";
 
-const Link = (props: LinkProps) => (
-  <ReactLink
-    {...props}
-    css={theme => ({
-      backgroundColor: "rgba(0, 0, 0, 0.8)",
-      fontSize: "1.5rem",
-      borderColor: theme.colors.text,
-      borderWidth: 2,
-      borderStyle: "solid",
-      borderRadius: 10,
-      padding: "0.5rem 2rem",
-      textAlign: "center",
-      width: "100%",
-      "&:hover": {
-        transition: "0.3s",
-        borderColor: "yellow",
-        color: "yellow",
-      },
-    })}
-  />
-);
+const Link = (props: LinkProps) => <ReactLink {...props} className={buttonStyle({ size: "lg", color: "outline" })} />;
 
 const HomePage = () => {
+  const { t } = useTranslation();
   return (
-    <div
-      css={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: "1rem",
-        padding: "2rem",
-      }}
-    >
-      <Link to={"/password-generator"}>PASSWORD GENERATOR</Link>
-      <Link to={"/star-rating"}>STARS RATING GENERATOR</Link>
+    <div className={linkContainerStyle}>
+      <Link to={"/password-generator"}>{t("password-generator.title")}</Link>
+      <Link to={"/star-rating"}>{t("star-rating.title")}</Link>
     </div>
   );
 };
