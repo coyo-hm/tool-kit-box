18a767bc393e4c840ca866df04cc5e0c85e236c4
COYO <dayeon.yoon@gmail.com>
2025-02-11 02:12:58 +0900

    feat: 다국어 지원 추가(react-i18n)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "@styles/reset.css";
+import "@locales/i18n";
+import App from "./App.tsx";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
