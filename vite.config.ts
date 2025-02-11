66c6aea2c0061f4b7a93a83477673b094844fa9d
COYO <dayeon.yoon@gmail.com>
2025-02-12 01:08:45 +0900

    fix: vite base 추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/tool-kit-box/",
   plugins: [react(), vanillaExtractPlugin(), tsconfigPaths(), svgr()],
 });
