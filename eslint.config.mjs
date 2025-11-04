9f697ab236901c73a6363fb2334e4f1524a40b39
COYO <dayeon.yoon@gmail.com>
2025-11-05 00:15:29 +0900

    chore: upgrade eslint to latest version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,16 @@
-export default {
-  extends: ["next/core-web-vitals", "next/typescript", "plugin:import/recommended", "plugin:import/typescript"],
-  plugins: ["import"],
-};
+import nextConfig from "eslint-config-next";
+import importPlugin from "eslint-plugin-import";
+
+export default [
+  ...nextConfig,
+  importPlugin.configs.recommended,
+  importPlugin.configs.typescript,
+  {
+    settings: {
+      "import/resolver": {
+        typescript: true,
+        node: true,
+      },
+    },
+  },
+];
